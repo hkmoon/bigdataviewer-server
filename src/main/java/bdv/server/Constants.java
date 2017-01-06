4cffec8bae65f824e1cdbcb8aec846bec5f3e188
HongKee Moon <hkmoon@me.com>
2017-01-06 18:02:02 +0100

    Update Constants for supporting public/domain contexts

--- src/main/java/bdv/server/Constants.java
+++ src/main/java/bdv/server/Constants.java
@@ -17,4 +17,18 @@ public class Constants
 	public static final int THUMBNAIL_WIDTH = 100;
 
 	public static final int THUMBNAIL_HEIGHT = 100;
+
+	// PUBLIC context
+	public static final String PUBLIC_DATASET_TAG_CONTEXT_NAME = "public/tag";
+
+	public static final String PUBLIC_DATASET_CONTEXT_NAME = "public/dataset";
+
+	// PRIVATE context
+	public static final String PRIVATE_DOMAIN = "private";
+
+	public static final String PRIVATE_DATASET_TAG_CONTEXT_NAME = PRIVATE_DOMAIN + "/tag";
+
+	public static final String PRIVATE_DATASET_CONTEXT_NAME = PRIVATE_DOMAIN + "/dataset";
+
+	public static final String PRIVATE_USER_CONTEXT_NAME = PRIVATE_DOMAIN + "/user";
 }
